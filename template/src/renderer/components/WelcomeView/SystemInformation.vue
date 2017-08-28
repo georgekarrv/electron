2c4d4755a2781ab3262d565448004ae5990e1898
Kylart <Kylart.dev@gmail.com>
2017-08-29 00:31:07 +0200

    Fix handlebars error and removed useless comps

--- template/src/renderer/components/WelcomeView/SystemInformation.vue
+++ template/src/renderer/components/WelcomeView/SystemInformation.vue
@@ -7,27 +7,27 @@
     <v-layout row wrap class="pa-3">
       <v-flex xs12 sm6 md4 class="item">
         <div class="name">Path:</div>
-        <div class="value">{{ path }}</div>
+        <div class="value">\{{ path }}</div>
       </v-flex>
       <v-flex xs12 sm6 md4 class="item">
         <div class="name">Name:</div>
-        <div class="value">{{ name }}</div>
+        <div class="value">\{{ name }}</div>
       </v-flex>
       <v-flex xs12 sm6 md4 class="item">
         <div class="name">Vue.js:</div>
-        <div class="value">{{ vue }}</div>
+        <div class="value">\{{ vue }}</div>
       </v-flex>
       <v-flex xs12 sm6 md4 class="item">
         <div class="name">Electron:</div>
-        <div class="value">{{ electron }}</div>
+        <div class="value">\{{ electron }}</div>
       </v-flex>
       <v-flex xs12 sm6 md4 class="item">
         <div class="name">Node:</div>
-        <div class="value">{{ node }}</div>
+        <div class="value">\{{ node }}</div>
       </v-flex>
       <v-flex xs12 sm6 md4 class="item">
         <div class="name">Platform:</div>
-        <div class="value">{{ platform }}</div>
+        <div class="value">\{{ platform }}</div>
       </v-flex>
     </v-layout>
   </v-card>
